b3e2b8a7709e528cb37f467126ee7277ad6bbed6
Ashok Patidar <flash.ria.developer@gmail.com>
2018-02-02 20:01:45 -0500

    configuring webpac

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+    entry: [
+        'webpack-dev-server/client?http://localhost:8080',
+        'webpack/hot/only-dev-server',
+        './src/index.js'
+    ],
+    module: {
+        loaders: [{
+            test: /\.js?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        }]
+    },
+    resolve: {
+        extensions: ['.jsx', '.js']
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        contentBase: './dist',
+        hot: true
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ]
+};
